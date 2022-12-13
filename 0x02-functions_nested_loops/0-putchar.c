50f1a10f2f7d5b5662288ea65a9534496af49086
PurpleThread <vanessa.manda4@gmail.com>
2022-12-13 13:23:52 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: '-putchar file'
+ * $Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	_putchar ('_');
+	_putchar ('p');
+	_putchar ('u');
+	_putchar ('t');
+	_putchar ('c');
+	_putchar ('h');
+	_putchar ('a');
+	_putchar ('r');
+	_putchar ('\n');
+	output (0);
+}
