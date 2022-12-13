6b2ca7c8c3fa83698e9d1d426e5349793adb4dd6
PurpleThread <vanessa.manda4@gmail.com>
2022-12-13 02:49:35 -0800

    4. Puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
-*/
+/**
+ * main - Entry point
+ *
+ * Description: 'printing a c file'
+ *
+ * Return: Always 0 (Success)
+ */
 
-*int main(void)
+int main(void)
 
 {
        	puts("\"Programming is like building a multilingual puzzle");
