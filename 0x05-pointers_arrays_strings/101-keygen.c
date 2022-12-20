974b37ff002b3113575cc5a5be55a61c8e4e656f
PurpleThread <vanessa.manda4@gmail.com>
2022-12-20 11:01:50 -0800

    11. Don't hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible passwords
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 48; a < 58; a++)
+	{
+		for (c = b; c < 58; c++)
+		{
+			if (a == b || b == c || a == c)
+			{
+				continue;
+			}
+			putchar(a);
+			putchar(b);
+			putchar(c);
+
+			if (a == 55 && b == 56 && c == 57)
+			{
+				break;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
