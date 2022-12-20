09ab06821afdad6fb232d6349b60f1f9e07367e7
PurpleThread <vanessa.manda4@gmail.com>
2022-12-20 10:25:00 -0800

    9. strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,21 +2,20 @@
 
 /**
  * *_strcpy - copies a string
- *
  * @*src: A pointerto a char that will be copied
  * @*dest: A pointer to a char that will be copied
  * Return: dest
  */
-
 char *_strcpy(char *dest, char *src)
 {
-	char i;
+	int i;
 
 	i = 0;
 
 	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
+		i++;
 	}
 	dest[i] = '\0';
 
