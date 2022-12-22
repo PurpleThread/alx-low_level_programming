22c64b048d575b58d0cb8331d28930222f4b6011
PurpleThread <vanessa.manda4@gmail.com>
2022-12-22 09:28:08 -0800

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings
+ * @dest: a destination string
+ * @src: A string to append
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int len =0, i;
+
+	while (dest[len])
+		len++;
+	for (i = 0, src[i] != '\0'; i ++)
+	{
+		dest[len] = scr[i];
+		len += 1;
+	}
+	dest [len] = '\0';
+	return (dest)
+}
