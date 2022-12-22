e56163c83dbe97bf4d82685959fa165fe76481f0
PurpleThread <vanessa.manda4@gmail.com>
2022-12-22 09:26:59 -0800

    A main.h file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c)
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -6,4 +10,5 @@ char *string_toupper(char *);
 void reverse_array(int *a, int n);
 char *cap_string(char *);
 char *leet(char *);
-int _putchar(char c)
+ 
+#endif
