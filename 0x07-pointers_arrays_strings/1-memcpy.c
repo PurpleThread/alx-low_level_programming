5a86f4fe5f29f0b6aa603d787a0dd4ce5b872590
PurpleThread <vanessa.manda4@gmail.com>
2023-01-03 07:05:33 -0800

    1. memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: memory area
+ * @src: another memory area
+ * @n: bytes
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
+
